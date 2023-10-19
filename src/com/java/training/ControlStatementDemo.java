5855a9e1e986cf9b0ec1b2a3a62797dc987b642a
ramanasaddi <ramana.mail@gmail.com>
2023-10-18 21:14:40 -0400

    Initial Commit

---
+++ src/com/java/training/ControlStatementDemo.java
@@ -0,0 +1,57 @@
+package com.java.training;
+
+public class ControlStatementDemo {
+
+    public static void main(String[] args) {
+
+        //if
+        //while
+        //for
+        //switch
+        //break
+        //foreach
+
+        int age=20;
+        if(age>18){
+            System.out.print("Age is greater than 18");
+        }
+
+        /*int number=13;
+        if(number%2==0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+        for(int j=1;j<=10;j++){
+            System.out.println(j);
+        }
+
+        int z=20;
+        switch(z){
+            case 10: System.out.println("10");break;
+            case 20: System.out.println("20");break;
+            case 30: System.out.println("30");break;
+            default:System.out.println("Not in 10, 20 or 30");
+        }
+
+        for(int x=1;x<=10;x++){
+            if(x==5){
+                break;
+            }
+            System.out.println(x);
+        }
+
+        int arr[]={12,23,44,56,78};
+        for(int k:arr){
+            System.out.println(k);
+        }*/
+
+    }
+}
---
+++ src/com/java/training/Exercise.java
@@ -0,0 +1,21 @@
+package com.java.training;
+
+public class Exercise {
+    public static void main(String[] args)
+    {
+        int i=35;
+        int j=42;
+      // Addition of 2 numbers
+        int x=i+j;
+        System.out.println("Sum of "+i+ "+ "+j+" = "+x);
+        // Multiplication Table of a number
+        j=8;
+        System.out.println("Table of "+j);
+        for (i=1;i <= 10; i++)
+        {
+            System.out.println(j+" x "+i+" = "+(j*i));
+        }
+
+        //
+    }
+}
