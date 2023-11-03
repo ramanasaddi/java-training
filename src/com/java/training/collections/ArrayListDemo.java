0544354ce01f2f475cf50e3b9561d6e23078014f
ramanasaddi <ramana.mail@gmail.com>
2023-11-02 21:03:47 -0400

    ArrayList for integers and String

---
+++ src/com/java/training/collections/ArrayListDemo.java
@@ -0,0 +1,51 @@
+package com.java.training.collections;
+
+import java.util.ArrayList; // import the ArrayList class
+import java.util.Collections;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>(); // Create an ArrayList object of Integers
+
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
+        // Integer -> class -
+        // int - simple - >
+
+        for(int i=0;i<myNumbers.size(); i++) {
+            System.out.println(myNumbers.get(i));
+        }
+
+        Collections.sort(myNumbers);  // Sort myNumbers
+
+        System.out.println("+++++++++++++++++++++");
+        for (Integer i : myNumbers) {
+            System.out.println(i);
+        }
+        System.out.println("+++++++++++++++++++++");
+
+        ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
+        Collections.sort(cars);  // Sort cars
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+    }
+}
